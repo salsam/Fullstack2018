2533a53193af765387a8805bfe8672ee8ed6c757
Sami <sami.salonen705@gmail.com>
2018-01-16 11:24:26 +0200

    Tehtävä 1.4

--- viikko1_app1/src/index.js
+++ viikko1_app1/src/index.js
@@ -12,47 +12,49 @@ const Osa = (props) => (
 const Osat = (props) => {
     console.log(props.osat)
     return (
-    <div>
-        <Osa osa={props.osat[0]} />
-        <Osa osa={props.osat[1]} />
-        <Osa osa={props.osat[2]} />
-    </div>
+        <div>
+            <Osa osa={props.osat[0]} />
+            <Osa osa={props.osat[1]} />
+            <Osa osa={props.osat[2]} />
+        </div>
     )
 }
 
 const Yhteensa = (props) => (
-    <p>yhteensä {props.summa} tehtävää</p>
+    <p>yhteensä {props.osat[0].tehtavia 
+        + props.osat[1].tehtavia 
+        + props.osat[2].tehtavia} tehtävää</p>
 )
 
 //const Yhteensa = (props) 
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    }
-    const osa2 = {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    }
-    const osa3 = {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
-    const osat = [osa1, osa2, osa3]
-    const summa=osa1.tehtavia+osa2.tehtavia+osa3.tehtavia
+    const osat = [
+        {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+        },
+        {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+        },
+        {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+        }
+    ]
 
-  return (
-    <div>
-      <Otsikko kurssi={kurssi} />
-      <Osat osat={osat}/>
-      <Yhteensa summa={summa} />
-    </div>
-  )
+    return (
+        <div>
+            <Otsikko kurssi={kurssi} />
+            <Osat osat={osat} />
+            <Yhteensa osat={osat} />
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 )
