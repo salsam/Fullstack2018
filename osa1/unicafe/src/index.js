b0dd3a56aeafdabaddcf6fd882b3b90ee8a560fd
Sami <sami.salonen705@gmail.com>
2018-01-21 14:37:30 +0200

    Teht 1.7 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -5,6 +5,18 @@ const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>{text}</button>
 )
 
+const keskiarvo = ({ hyva,neutraali,huono }) => {
+    const yhteensa=hyva+neutraali+huono
+    if (yhteensa === 0) return 0
+    return Math.round(1000*(hyva-huono)/yhteensa)/1000
+}
+
+const positiivisuusProsentti = ({ hyva, neutraali, huono}) => {
+    const yhteensa=hyva+neutraali+huono
+    if (yhteensa === 0) return 0
+    return Math.round(1000*hyva/yhteensa)/10
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props)
@@ -19,8 +31,6 @@ class App extends React.Component {
     annaNeutraaliaPalautetta = () => this.setState({neutraali: this.state.neutraali + 1})
     annaHuonoaPalautetta = () => this.setState({huono: this.state.huono + 1})
     
-
-
     render() {
         return (
             <div>
@@ -32,6 +42,8 @@ class App extends React.Component {
                 <div>hyvä {this.state.hyva}</div>
                 <div>neutraali {this.state.neutraali}</div>
                 <div>huono {this.state.huono}</div>
+                <div>keskiarvo {keskiarvo(this.state)}</div>
+                <div>positiivisia {positiivisuusProsentti(this.state)}%</div>
             </div>
         )
     }
