3a1a2ee7b1dd3ca191740610b6d7c7366f33f2ca
Sami <sami.salonen705@gmail.com>
2018-01-21 15:04:44 +0200

    Korjattu vielä hieman t1.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -5,17 +5,32 @@ const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>{text}</button>
 )
 
-const keskiarvo = ({ hyva,neutraali,huono }) => {
-    const yhteensa=hyva+neutraali+huono
+const keskiarvo = ({ hyva, neutraali, huono }) => {
+    const yhteensa = hyva + neutraali + huono
     if (yhteensa === 0) return 0
-    return Math.round(1000*(hyva-huono)/yhteensa)/1000
+    return Math.round(1000 * (hyva - huono) / yhteensa) / 1000
 }
 
-const positiivisuusProsentti = ({ hyva, neutraali, huono}) => {
-    const yhteensa=hyva+neutraali+huono
+const positiivisuusProsentti = ({ hyva, neutraali, huono }) => {
+    const yhteensa = hyva + neutraali + huono
     if (yhteensa === 0) return 0
-    return Math.round(1000*hyva/yhteensa)/10
+    return Math.round(1000 * hyva / yhteensa) / 10
 }
+
+const Statistic = ({ value, text, textafter}) => (
+    <div>{text} {value} {textafter}</div>
+)
+
+const Statistics = (props) => (
+    <div>
+        <h1>statistiikka</h1>
+        <Statistic value={props.hyva} text="hyvä"/>
+        <Statistic text="neutraali" value={props.neutraali}/>
+        <Statistic text="huono" value={props.huono}/>
+        <Statistic text="keskiarvo" value={keskiarvo(props)}/>
+        <Statistic text="positiivisia" value={positiivisuusProsentti(props)} textafter="%" />
+    </div>
+)
 
 class App extends React.Component {
     constructor(props) {
@@ -27,23 +42,18 @@ class App extends React.Component {
         }
     }
 
-    annaHyvaaPalautetta = () => this.setState({hyva: this.state.hyva + 1})
-    annaNeutraaliaPalautetta = () => this.setState({neutraali: this.state.neutraali + 1})
-    annaHuonoaPalautetta = () => this.setState({huono: this.state.huono + 1})
-    
+    annaHyvaaPalautetta = () => this.setState({ hyva: this.state.hyva + 1 })
+    annaNeutraaliaPalautetta = () => this.setState({ neutraali: this.state.neutraali + 1 })
+    annaHuonoaPalautetta = () => this.setState({ huono: this.state.huono + 1 })
+
     render() {
         return (
             <div>
                 <h1>anna palautetta</h1>
-                <Button handleClick={this.annaHyvaaPalautetta} text="hyvä"/>
-                <Button handleClick={this.annaNeutraaliaPalautetta} text="neutraali"/>
-                <Button handleClick={this.annaHuonoaPalautetta} text="Huono"/>
-                <h1>statistiikka</h1>
-                <div>hyvä {this.state.hyva}</div>
-                <div>neutraali {this.state.neutraali}</div>
-                <div>huono {this.state.huono}</div>
-                <div>keskiarvo {keskiarvo(this.state)}</div>
-                <div>positiivisia {positiivisuusProsentti(this.state)}%</div>
+                <Button handleClick={this.annaHyvaaPalautetta} text="hyvä" />
+                <Button handleClick={this.annaNeutraaliaPalautetta} text="neutraali" />
+                <Button handleClick={this.annaHuonoaPalautetta} text="Huono" />
+                {Statistics(this.state)}
             </div>
         )
     }
