4b954a39ad7d2276ea61b2cbc196a24778d5436a
Sami <sami.salonen705@gmail.com>
2018-02-11 14:43:56 +0200

    4.6 raakaversio valmis

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,4 +1,3 @@
-
 const dummy = (blogs) => {
     return 1
 }
@@ -16,16 +15,30 @@ const mostBlogs = (blogs) => {
     if (blogs.length === 0) {
         return null
     }
+    const ams = blogs.reduce((names,blog) => {
+        blog.author in names ? names[blog.author]++ : names[blog.author]=1
+        return names
+    }, {})
 
+    const best = { author: "A", blogs: -1}
 
+    for (name in ams) {
+        if (ams[name] > best.blogs) {
+            best.author=name,
+            best.blogs=ams[name]
+        }
+    }
+
+    return best
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((acc, cum) => acc.likes + cum.likes, 0)
+    return blogs.map(blogs => blogs.likes).reduce((acc, cum) => acc + cum, 0)
 }
 
 module.exports = {
     dummy,
     favouriteBlog,
+    mostBlogs,
     totalLikes
 }
