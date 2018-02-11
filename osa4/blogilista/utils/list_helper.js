beb8eac252884a6b2063e550cc03c535da54d6ff
Sami <sami.salonen705@gmail.com>
2018-02-11 13:45:07 +0200

    4.4 valmis

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.map(blogs => blogs.likes).reduce((acc,cum) => acc+cum,0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
