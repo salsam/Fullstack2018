163c310c246dda313ef137eb181c744068c5acf2
Sami <sami.salonen705@gmail.com>
2018-02-11 14:07:37 +0200

    4.5 valmis

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -100,3 +100,111 @@ describe('total likes', () => {
         expect(listHelper.totalLikes(blogs.concat(negativeBlog))).toBe(-6)
     })
 })
+
+describe('favouriteBlog', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const blogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+            __v: 0
+        }
+    ]
+
+    const negativeBlog = {
+        _id: "5a422bc61b54a676234d17f4",
+        title: "Typo wars",
+        author: "Robert R. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: -42,
+        __v: 0
+    }
+
+    test('when list has only one blog equals  that', () => {
+        expect(listHelper.favouriteBlog(listWithOneBlog)).toEqual(listWithOneBlog[0])
+    })
+
+    test('when list has no blogs return undefined', () => {
+        expect(listHelper.favouriteBlog([])).toBeNull()
+    })
+
+    test('when list has many elements equals the on with most likes', () => {
+        expect(listHelper.favouriteBlog(blogs)).toEqual({
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        })
+    })
+
+    test('when list has single element with negative likes equals that', () => {
+        expect(listHelper.favouriteBlog([negativeBlog])).toEqual(negativeBlog)
+    })
+
+    test('when list has multiple elements summing to negative total like equals blogs wiht most likes', () => {
+        expect(listHelper.favouriteBlog(blogs.concat(negativeBlog))).toEqual({
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        })
+    })
+    
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,11 +3,21 @@ const dummy = (blogs) => {
     return 1
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    const mostLikes = blogs.map(blog => blog.likes)
+        .reduce((acc, cum) => Math.max(acc, cum), blogs[0].likes)
+    return blogs.find(blog => blog.likes === mostLikes)
+}
+
 const totalLikes = (blogs) => {
-    return blogs.map(blogs => blogs.likes).reduce((acc,cum) => acc+cum,0)
+    return blogs.map(blogs => blogs.likes).reduce((acc, cum) => acc + cum, 0)
 }
 
 module.exports = {
     dummy,
+    favouriteBlog,
     totalLikes
 }
