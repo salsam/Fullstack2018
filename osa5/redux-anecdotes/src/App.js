baf5aa522ef26eabe8a6b17605476deeaaf74a85
Sami <sami.salonen705@gmail.com>
2018-02-21 17:55:30 +0200

    Viikko 5 valmis

---
+++ osa5/redux-anecdotes/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  vote = (id) => () => {
+    const action = {
+      type: 'vote',
+      data: {
+        id: id
+      }
+    }
+    this.props.store.dispatch(action)
+  }
+
+  addAnecdote = (event) => {
+    event.preventDefault()
+    console.log(event.target.anecdote.value)
+    this.props.store.dispatch({
+      type: 'add',
+      data: event.target.anecdote.value
+    })
+  }
+
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={this.vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form onSubmit={this.addAnecdote}>
+          <div><input name="anecdote" /></div>
+          <button>create</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ osa5/redux-anecdotes/src/reducer.js
@@ -0,0 +1,40 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch (action.type) {
+    case 'vote':
+      const id = action.data.id
+      const old = state.find(anecdote => anecdote.id === id)
+      const updated = { ...old, votes: old.votes + 1 }
+      const newState = [...state.filter(anecdote => anecdote.id !== id), updated]
+      return newState.sort((a, b) => a.votes < b.votes)
+    case 'add':
+      return [...state, asObject(action.data)]
+    default:
+      return state
+  }
+}
+
+export default reducer
